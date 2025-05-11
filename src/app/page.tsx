f701f638e525e2ec28718c5c154d23aeb99b6314
Vinay Thakor <vinay.th.24@gmail.com>
2025-05-11 12:49:00 +0530

    Global CSS setup for theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-(family-name:--font-geist-sans)">
-      <Button>Click Me</Button>
+      <h1>Resumir</h1>
+      <Button variant={'outline'}>Click me i am button</Button>
     </div>
   );
 }
