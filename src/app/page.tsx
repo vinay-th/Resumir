e74c3ea8fd0094dcec741bdb8c1eb685cbb69cf9
Vinay Thakor <vinay.th.24@gmail.com>
2025-05-13 13:50:37 +0530

    Add BgGradient component and integrate it into the homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import FeatureSection from '@/components/home/FeatureSection';
 import HeroSection from '@/components/home/HeroSection';
 import DemoSection from '@/components/home/DemoSection';
+import BgGradient from '@/components/common/BgGradient';
 
 export default function Home() {
   return (
-    <div className="relative w-full items-center justify-items-center">
-      <HeroSection />
-      <FeatureSection />
+    <div className="relative w-full">
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
       <DemoSection />
+      <FeatureSection />
       {/* <PricingSection> */}
       {/* <CTASection /> */}
     </div>
